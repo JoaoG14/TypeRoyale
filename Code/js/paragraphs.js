5dd50e01b887f1004c46612cbe299edad5dd2cd6
João <joaog.alves14@gmail.com>
2023-08-23 14:50:45 -0300

    design changes

--- Code/js/paragraphs.js
+++ Code/js/paragraphs.js
@@ -1,4 +1,4 @@
 const paragraphs = [
-    "tudo existir circuito realizar tanto porque o chegar saber conseguir problema ficar isso parte pai vez certo pequeno achar tão trabalho poder circuito acreditar possível de pouco verdade tudo primeiro importante",
+    "tudo existir circuito realizar tanto porque o chegar saber conseguir problema ficar isso parte pai vez certo pequeno achar tão trabalho poder circuito acreditar possível de pouco verdade tudo primeiro ",
     
 ];
