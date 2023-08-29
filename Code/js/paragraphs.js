ba0fe5d42d404ad5ec1389faf1faf56f5f481e9f
João <joaog.alves14@gmail.com>
2023-08-29 14:57:47 -0300

    add new paragraphs

--- Code/js/paragraphs.js
+++ Code/js/paragraphs.js
@@ -1,4 +1,10 @@
 const paragraphs = [
-    "tudo existir circuito realizar tanto porque o chegar saber conseguir problema ficar isso parte pai vez certo pequeno achar tão trabalho poder circuito acreditar possível de pouco verdade tudo primeiro ",
-    
+  "impose grasp bet gang venomous background among junk animated dial ruby dome tomatoes bewildered greedy oatmeal same gray appear wheel include numerous nude entertainment ",
+  "baboon brothers graveyard beat umbrella handler harness rubber block special between true organization fake stitch enchanting flag rust resemble agenda oceanic subway also curve", 
+  "dependent curb creeper bitterness spend gargoyle stew roof rabid bent witness somber story colorful bottom justify abrupt abundant recondite acidic shear skinny curious retreat ", 
+  "inlay dexterity salad protect knuckles weigh manage flowery like neurotic bears eat scintillating cynic subtract deprive blended truculent voracious prosecute fried revolt ", 
+  "pyramids hover stomach needy greasy being pet hunter interest dogs bald cooing analysis vibrator glass infect fog device sphere engineering glumly band bless bomber failure ",
+  "eternity impair distancing heavyweight gangland stink puzzled entropy bury pigeon intruder sawdust deafening hill factual barbarous shrill artist scarify wistful elongation southern serum ",
+  "violet armchair diminish pneumatic spike camera fanatic prescribe ill-fated blemish constant succinct ebony pointless caress blankly tragic observe quizzical son ankle betray rastle",
+  "story little caramel weed atten autonomous innovate contain apparently front lawyer patience flimsy gratis meek baseline research oafish rings falcon apartment willing kittens  "
 ];
